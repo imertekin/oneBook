cc1291af76866cf9166c7c698378590100ee3094
ilkay murat ertekin <iertekinx@gmail.com>
2022-01-01 17:51:19 +0300

    Add admin panel

--- Api/admin.py
+++ Api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from Api.models import Book,Borrow
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'ISBN','Author','is_avaible','name']
+    search_fields=['borrow_book__user__username','Author']
+ 
+@admin.register(Borrow)
+class BorrowAdmin(admin.ModelAdmin):
+    list_display = ['id', 'Book','user']
+ 
