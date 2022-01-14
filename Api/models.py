10486e3496ab7dd9c4782b2033ace467d7e0f9ad
ilkay murat ertekin <iertekinx@gmail.com>
2022-01-14 21:20:59 +0300

    Add like and comment model

--- Api/models.py
+++ Api/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class Book(models.Model):
     ISBN=models.CharField(max_length=13,unique=True)
+    image=models.ImageField(null=True,blank=True,upload_to='%Y/%m/%d')
     name=models.CharField(max_length=50)
     Author=models.CharField(max_length=50)
     Publisher=models.CharField(max_length=50)
     Publication_date=models.DateField()
     Genres=models.CharField(max_length=50)
     Print_length=models.PositiveSmallIntegerField()
-    is_avaible=models.BooleanField(default=True)
 
 
 
@@ -18,16 +18,31 @@ class Book(models.Model):
         return f'{self.name} by {self.Author}'
 
     @property
-    def takedBy(self):
-        return self.borrow_book.user.username
+    def _likes_count(self):
+        return self.like.count()
+
+    @property
+    def _comments(self):
+        return self.books_comment
     
         
 
 
-class Borrow(models.Model):
-    Book=models.OneToOneField(Book,on_delete=models.CASCADE,related_name='borrow_book')
-    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='borrow_user')
+class Like(models.Model):
+    book=models.ForeignKey(Book,on_delete=models.CASCADE,related_name='like')
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='liker')
+    created_at=models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self) -> str:
-        return f'{self.Book.name} borrowed by {self.user.username}'
+        return f'{self.book.name} liked by {self.user.username}'
+
+
+class Comment(models.Model):
+    book=models.ForeignKey(Book,on_delete=models.CASCADE,related_name='books_comment')
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='comment_owner')
+    content=models.TextField(max_length=140,blank=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f" {self.user.username} 's comment  "
