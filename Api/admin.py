ac46dd96bc8aafcb97ffded41b253aeee091c53b
ilkay murat ertekin <iertekinx@gmail.com>
2022-01-14 22:41:13 +0300

    Add admin register

--- Api/admin.py
+++ Api/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
 
-from Api.models import Book,Borrow
+from Api.models import Book, Comment,Like
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['id', 'ISBN','Author','is_avaible','name']
-    search_fields=['borrow_book__user__username','Author']
+    list_display = ['id', 'ISBN','Author','name']
+    search_fields=['Author']
  
-@admin.register(Borrow)
-class BorrowAdmin(admin.ModelAdmin):
-    list_display = ['id', 'Book','user']
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'book','user']
  
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'book','user','content']
+
+from rest_framework_simplejwt import token_blacklist
+
+class OutstandingTokenAdmin(token_blacklist.admin.OutstandingTokenAdmin):
+
+    def has_delete_permission(self, *args, **kwargs):
+        return True # or whatever logic you want
+
+admin.site.unregister(token_blacklist.models.OutstandingToken)
+admin.site.register(token_blacklist.models.OutstandingToken, OutstandingTokenAdmin)
