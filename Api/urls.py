6e560b09b04e4177cca8fcd708f8c8eaa9f53fdd
ilkay murat ertekin <iertekinx@gmail.com>
2022-01-14 22:40:33 +0300

    Add urls

--- Api/urls.py
+++ Api/urls.py
@@ -2,14 +2,18 @@ from django.urls import include,path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import BookViewset,BorrowListCreateView, UserViewset
+from .views import BookViewset,LikeViewSet, UserViewset,ProfileView,CommentView
 
 router = DefaultRouter()
+
 router.register(r'books', BookViewset, basename='books')
-router.register(r'borrows',BorrowListCreateView,basename='borrows')
+router.register(r'like',LikeViewSet,basename='like')
+router.register(r'comment',CommentView,basename='comment')
 router.register(r'users',UserViewset,basename='users')
+router.register(r'profile',ProfileView,basename='profile')
 urlpatterns = [
-    path('',include(router.urls))
+    path('',include(router.urls)),
+    
 ]
 
 
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
-
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include,re_path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -10,6 +13,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from Api.views import RegisterView
 
 
 schema_view = get_schema_view(
@@ -33,6 +37,7 @@ urlpatterns = [
     path('api/', include('Api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/',RegisterView.as_view(),name='auth-register'),
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
@@ -44,3 +49,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
