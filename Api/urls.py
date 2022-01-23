03b80e2f31f87038fc9e16ee71bdaec78ec3c576
ilkay murat ertekin <iertekinx@gmail.com>
2022-01-23 20:13:34 +0300

    Add booklist route

--- Api/urls.py
+++ Api/urls.py
@@ -2,12 +2,13 @@ from django.urls import include,path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import BookViewset,LikeViewSet, UserViewset,ProfileView,CommentView
+from .views import BookViewset,LikeViewSet, UserViewset,ProfileView,CommentView,BookListViewSet
 
 router = DefaultRouter()
 
 router.register(r'books', BookViewset, basename='books')
 router.register(r'like',LikeViewSet,basename='like')
+router.register(r'booklist',BookListViewSet,basename='booklist')
 router.register(r'comment',CommentView,basename='comment')
 router.register(r'users',UserViewset,basename='users')
 router.register(r'profile',ProfileView,basename='profile')
