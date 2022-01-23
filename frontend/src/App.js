9ce0cb91c28c500dcb1342ff74050e37f257f6b2
ilkay murat ertekin <iertekinx@gmail.com>
2022-01-23 20:59:09 +0300

    Fix user state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,33 +20,35 @@ import Profile from "./components/Profile";
 function App() {
   const [Books, setBooks] = useState([]);
   const [isLoggin, setIsLoggin] = useState("");
-  const [user, setUser] = useState({username:""})
+  const [user, setUser] = useState({
+    username: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+    comments: [],
+    mybooks: [],
+    likes: [],
+  });
 
-  const access=localStorage.getItem('access_token')
+  const access = localStorage.getItem("access_token");
   useEffect(() => {
-    
-    access ? setIsLoggin(true):setIsLoggin(false)
-  }, [access])
-
+    access ? setIsLoggin(true) : setIsLoggin(false);
+  }, [access]);
 
   useEffect(() => {
-    
     const getBooks = async () => {
       try {
         const res = await axios.get("/books/");
         setBooks(...[res.data]);
       } catch (error) {}
-    }
-    const profile = async()=>{
+    };
+    const profile = async () => {
       try {
-        const res= await axios.get("/profile/")
-      setUser(...res.data)
-      } catch (error) {
-        
-      }
-      
-  }
-  profile()
+        const res = await axios.get("/profile/");
+        setUser(...res.data);
+      } catch (error) {}
+    };
+    profile();
     getBooks();
   }, []);
 
@@ -60,11 +62,14 @@ function App() {
             path="/login"
             element={<LoginPage setIsLoggin={setIsLoggin} />}
           />
-           <Route
+          <Route
             path="/register"
             element={<RegisterPage setIsLoggin={setIsLoggin} />}
           />
-          <Route path="/my-profile" element={<Profile user={user} setUser={setUser} />}/>
+          <Route
+            path="/my-profile"
+            element={<Profile user={user} setUser={setUser} />}
+          />
           <Route path="*" element={<Navigate to="/404" />} />
           <Route path="/404" element={<NotFound />} />
           <Route path="/books/:id" element={<BookDetail user={user} />} />
