b8bbf4308bf31565f130944b31f23287bc36edc4
ilkay murat ertekin <iertekinx@gmail.com>
2022-01-23 19:53:41 +0300

    Add formik and login page

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,26 +1,25 @@
-import { Formik, Form,ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage} from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-export default function LoginPage({setIsLoggin}) {
+export default function LoginPage({ setIsLoggin }) {
+  let errormsg = "";
 
-    let errormsg=''
-
-    const handleOnSubmit= async(values,actions)=>{
-        try {
-            const res = await axios.post("http://127.0.0.1:8000/api/token/",
-            {username:values.username,
-            password:values.password})
-            localStorage.setItem('access_token', res.data.access);
-            localStorage.setItem('refresh_token', res.data.refresh);
-            setIsLoggin(true)
-            window.location.href = '/'
-          } catch (error) {
-          error.response.status===401&& (
-            errormsg=<>Username or Password Wrong</>
-          )
-        
-    }}
+  const handleOnSubmit = async (values, actions) => {
+    try {
+      const res = await axios.post("http://127.0.0.1:8000/api/token/", {
+        username: values.username,
+        password: values.password,
+      });
+      localStorage.setItem("access_token", res.data.access);
+      localStorage.setItem("refresh_token", res.data.refresh);
+      setIsLoggin(true);
+      window.location.href = "/";
+    } catch (error) {
+      error.response.status === 401 &&
+        (errormsg = <>Username or Password Wrong</>);
+    }
+  };
 
   return (
     <div className="login-container">
@@ -30,9 +29,9 @@ export default function LoginPage({setIsLoggin}) {
           password: "",
         }}
         validationSchema={Yup.object({
-          username: Yup.string().required("Username Required"),
+          username: Yup.string().required("Username required"),
           password: Yup.string()
-            .min(5, "Too Short!")
+            .min(5, "Too short !")
             .required("Password required"),
         })}
         onSubmit={handleOnSubmit}
@@ -48,40 +47,33 @@ export default function LoginPage({setIsLoggin}) {
           handleBlur,
         }) => {
           return (
-              
             <Form>
-                <img src="assets\images\monster.png" alt="Logo" />
-                {errormsg}
-                <input
-                  id="username"
-                  type="text"
-                  placeholder="Username"
-                  value={values.username}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  
-                />
-                
-                {errors.username && touched.username && (
-                  <div style={{color:"red",fontSize:"15px"}}>{errors.username}</div>
-                )}
-                
-                
-                <input
-                  id="password"
-                  type="password"
-                  placeholder="Password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                {errors.password && touched.password && (
-                  <div>{errors.password}</div>
-                )}
+              <img src="assets\images\monster.png" alt="Logo" />
+              {errormsg}
+              <Field
+                id="username"
+                type="text"
+                placeholder="Username"
+                value={values.username}
+                onChange={handleChange}
+                onblur={handleBlur}
+              />
 
-                <button type="submit" disabled={!dirty || isSubmitting}>
-                  Login
-                </button>
+                 <ErrorMessage name="username" />
+
+              <Field
+                id="password"
+                type="password"
+                placeholder="Password"
+                value={values.password}
+                onChange={handleChange}
+                onblur={handleBlur}
+              />
+              <ErrorMessage name="password" />
+
+              <button type="submit" disabled={!dirty || isSubmitting}>
+                Login
+              </button>
             </Form>
           );
         }}
