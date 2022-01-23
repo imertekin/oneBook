f2cf7ffa594a4be255b16f3ee22103c1df783bce
ilkay murat ertekin <iertekinx@gmail.com>
2022-01-23 19:54:04 +0300

    Fix book list

--- frontend/src/components/Booklist.js
+++ frontend/src/components/Booklist.js
@@ -3,11 +3,23 @@ import { Link } from "react-router-dom";
 export default function Booklist({ Books }) {
   return (
     <>
-
       <div className="container-list">
         {Books.map((book) => (
           <Link key={book.id} to={`/books/${book.id}`}>
-          <img height="400px" width="265px" src={book.image} alt="" />
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img height="400px" width="265px" src={book.image} alt="" />
+                </div>
+                <div className="flip-card-back">
+                  <h1>{book.name}</h1>
+                  <p>ISBN: {book.ISBN}</p>
+                  <p>Author: {book.Author}</p>
+                  <p>Publisher: {book.Publisher}</p>
+                  <p>Genres: {book.Genres}</p>
+                </div>
+              </div>
+            </div>
           </Link>
         ))}
       </div>
